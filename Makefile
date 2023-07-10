4f3640be4d07c1105fa5776406d33128dd8a1cdb
juan-bolivar <juan.bolivar@languagetool.org>
2023-07-10 10:34:05 +0200

    updated directory structure

--- Makefile
+++ Makefile
@@ -1,23 +1,30 @@
 .PHONY: install run stop uninstall loadtest
 
-install:
+install-registry:
 	helm repo add twuni https://helm.twun.io
 	helm repo update
 	kubectl create namespace docker-registry
 	helm install docker-registry twuni/docker-registry --namespace docker-registry
 
-run:
+
+install-dragonfly:
+	helm repo add dragonfly https://dragonflyoss.github.io/helm-charts/
+	helm install --create-namespace --namespace dragonfly-system dragonfly dragonfly/dragonfly -f dragonfly/values.yaml
+	kubectl -n dragonfly-system wait --for=condition=ready --all --timeout=10m pod
+
+
+run-registry:
 	sleep 3
 	nohup kubectl port-forward $$(kubectl get pods --namespace docker-registry -l "app=docker-registry,release=docker-registry" -o jsonpath="{.items[0].metadata.name}") 8080:5000 --namespace docker-registry &
 
-stop:
+stop-registry:
 	kill $$(ps aux | grep 'kubectl port-forward' | awk '{print $$2}')
 
-uninstall:
+uninstall-registry:
 	helm uninstall docker-registry --namespace docker-registry
 	kubectl delete namespace docker-registry
 
 loadtest:
 	docker pull ubuntu:latest
-	poetry run locust -f load_test.py --host=https://$$(awk -F "=" '/^username/ {print $$2}' config.ini):$$(awk -F "=" '/^password/ {print $$2}' config.ini)@$$(awk -F "=" '/^host/ {print $$2}' config.ini)
+	cd docker-registry && poetry install && poetry run locust -f load_test_registry.py --host=https://$$(awk -F "=" '/^username/ {print $$2}' config.ini):$$(awk -F "=" '/^password/ {print $$2}' config.ini)@$$(awk -F "=" '/^host/ {print $$2}' config.ini)
 
--- docker-registry/load_test_registry.py
+++ docker-registry/load_test_registry.py
@@ -1,15 +1,24 @@
-
+import json
 import configparser
 import subprocess
 import time
 from locust import User, task
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 registry_host = config.get('credentials', 'host')
 registry_host = registry_host.split("//")[-1]
 
+
+
+
 class DockerUser(User):
+
+    def get_image_size(self, image_name):
+        output = subprocess.check_output(['docker', 'image', 'inspect', image_name])
+        image_info = json.loads(output)
+        return image_info[0]['Size']
 
     @task
     def pull_image(self):
@@ -17,11 +26,12 @@ class DockerUser(User):
         start_time = time.time()
         subprocess.check_output(['docker', 'pull', image])
         total_time = int((time.time() - start_time) * 1000)  # converting to milliseconds
+        image_size = self.get_image_size(image)
         self.environment.events.request.fire(
             request_type='DOCKER PULL',
             name=image,
             response_time=total_time,
-            response_length=0,
+            response_length=image_size,
         )
 
     @task
@@ -32,10 +42,10 @@ class DockerUser(User):
         subprocess.check_output(['docker', 'tag', local_image, remote_image])
         subprocess.check_output(['docker', 'push', remote_image])
         total_time = int((time.time() - start_time) * 1000)  # converting to milliseconds
+        image_size = self.get_image_size(local_image)
         self.environment.events.request.fire(
             request_type='DOCKER PUSH',
             name=remote_image,
             response_time=total_time,
-            response_length=0,
+            response_length=image_size,
         )
-
